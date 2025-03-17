084eb130beb5194a9c6bbf5f3bb97dc6b6e134fc
Jan <zimniak.janek@outlook.com>
2025-03-17 15:12:22 +0100

    6. Zastąpienie literałów przez deklaracje stałych

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,13 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
+    public static final int NUMBERS_COUNT = 12;
+    public static final int ERROR_RETURN = -1;
+    public static final int FULL_SIZE = 11;
 
-    public int total = -1;
+    private int[] numbers = new int[NUMBERS_COUNT];
+
+    public int total = ERROR_RETURN;
 
     public void countIn(int in) {
         if (!isFull())
@@ -12,22 +16,22 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == ERROR_RETURN;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == FULL_SIZE;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return -1;
+            return ERROR_RETURN;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
+            return ERROR_RETURN;
         return numbers[total--];
     }
 
